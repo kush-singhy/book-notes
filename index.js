a8927a4c4106627177158da803f6d906b771b34a
Kush Singhy <kush.singhy@westpac.com.au>
2024-10-25 10:03:14 +1100

    Environment variables for database

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const agent = new https.Agent({
 const apiKey = process.env.API_KEY;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "library",
-  password: "123456",
-  port: 5432,
-});
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
 
 db.connect();
 
